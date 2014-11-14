77bff0fd524d070e420034ac94569e12f353c9a7
Daniel Paulus <d.paulus@gmail.com>
2014-11-14 14:56:49 +0100

    upgrade version to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'daniel@inuits.eu'
 license 'none'
 description 'Installs/Configures logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.3.0'
 
 depends 'yum'
